b9f18734dcfc24110de7fae0a2e6fd5b0c19200f
Vitor Rangel <vitorrangel@ciandt.com>
2022-07-27 20:41:25 -0300

    Alterações na forma do dynamoDb

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,15 +1,25 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
 
   const {title}= JSON.parse(event.body);
   const now  = new Date();
 
   const auction = {
-    
+    id: uuid(),
     title,
     status: "OPEN",
     createAt: now.toISOString(),
-
   };
+  //significa o seguinte, o await, "Espere esse código ser rodado" e ao final temos uma promisse que é promessa
+  await dynamodb.put({
+    TableName: 'AuctionsTable',
+    Item: auction,
+  }).promise();
+
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
